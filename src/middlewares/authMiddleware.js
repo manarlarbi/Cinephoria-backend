a3c62acee24880995665152c44344cfd35f5a45b
manar larbi <160422398+manarlarbi@users.noreply.github.com>
2025-03-27 22:47:47 +0100

    création de middleware pour gérer les droit d'accès

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt=require("jsonwebtoken");
+const SECRET_KEY="manar_clef";
+function verifyToken(req,res, next){
+    const authHeader =req.headers.authorization;
+    if(!authHeader||!authHeader.startsWith("Bearer ")){
+        return res.status(401).json({error: "token manquant invalide"});
+    }
+    const token =authHeader.split(" ")[1];
+    try {
+        const decoded = jwt.verify(token, SECRET_KEY);
+        req.user = decoded; 
+        next(); 
+      } catch (err) {
+        return res.status(401).json({ error: "Token invalide ou expiré" });
+      }
+} 
+function isAdmin(req,res,next){
+  if(req.user && req.user.role==="Administrateur"){
+    next();
+  }else{
+    return res.status(403).json({ error: "Accès refusé : réservé à l'administrateur" });
+  }
+}
+module.exports={verifyToken,isAdmin};
